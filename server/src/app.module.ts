72504ab19e54d015f4ec8f2da5ec825e7a7611c2
MAUREL Valentin <valentin1.maurel@epitech.eu>
2023-12-07 17:13:47 +0100

    feat(server): :card_file_box: setup mysql db + counter (PoC)
    setup the docker-compose file with mysql and integrate into NestJS server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CounterModule } from './modules/counter/counter.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql', // docker-compose.yml
+      host: 'db', // docker-compose.yml
+      port: 3306, // docker-compose.yml
+      username: 'user', // docker-compose.yml
+      password: 'password', // docker-compose.yml
+      database: 'db', // docker-compose.yml
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    CounterModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/modules/counter/entities/counter.entity.ts
@@ -0,0 +1,10 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Counter {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ default: 0 })
+  count: number;
+}
