0d5e951e76f872cc5538d80300e09629823c8ec2
Enzo <enzo.pfeiffer@epitech.eu>
2023-12-08 15:32:33 +0100

    feat(pages): home menu started and pages created

--- mobile/lib/pages/home/home.dart
+++ mobile/lib/pages/home/home.dart
@@ -1,11 +1,87 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 2;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: _buildPage(_currentIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.black,),
+            label: 'My AREA',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, color: Colors.black,),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle, color: Colors.black,),
+            label: 'Create',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment, color: Colors.black,),
+            label: 'Activity',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, color: Colors.black,),
+            label: 'Profile',
+          ),
+        ],
+        unselectedLabelStyle: const TextStyle(color: Colors.black),
+      ),
+    );
+  }
+
+  Widget _buildPage(int index) {
+    switch (index) {
+      case 0:
+        // Page pour 'My AREA'
+        return const Center(
+          child: Text('My AREA Page'),
+        );
+      case 1:
+        // Page pour 'Explore'
+        return const Center(
+          child: Text('Explore Page'),
+        );
+      case 2:
+        // Page pour 'Create'
+        return const Center(
+          child: Text('Create Page'),
+        );
+      case 3:
+        // Page pour 'Activity'
+        return const Center(
+          child: Text('Activity Page'),
+        );
+      case 4:
+        // Page pour 'Profile'
+        return const Center(
+          child: Text('Profile Page'),
+        );
+      default:
+        return Container();
+    }
   }
 }
--- mobile/lib/pages/login/login.dart
+++ mobile/lib/pages/login/login.dart
@@ -4,53 +4,136 @@ import 'package:mobile/pages/home/home.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('IFTTT Login'),
+        title: const Text(
+          'AREA',
+          style: TextStyle(fontSize: 100.0, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        toolbarHeight: 200.0,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const TextField(
-              decoration: InputDecoration(labelText: 'Email'),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
             ),
-            const TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                obscureText: true,
+              ),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
                   context,
-                MaterialPageRoute(builder: (context) => const HomePage()),
+                  MaterialPageRoute(builder: (context) => const HomePage()),
                 );
               },
-              child: const Text('Login'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.black,
+                onPrimary: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              child: const Text(
+                'Login',
+                style: TextStyle(fontSize: 40.0),
+              ),
             ),
             TextButton(
               onPressed: () {
-                // Ajoutez ici la logique d'inscription
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                );
               },
+              style: TextButton.styleFrom(
+                primary: Colors.black,
+              ),
               child: const Text('Sign Up'),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Ajoutez ici la logique d'authentification avec Google
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                );
               },
-              child: const Text('Login with Google'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.black,
+                onPrimary: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                // minimumSize: const Size(50, 20), ca ne fonctionne pas
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/logo/google_logo.png',
+                    height: 20.0,
+                  ),
+                  const SizedBox(width: 8.0),
+                  const Text(
+                    'Login with Google',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ],
+              ),
             ),
             ElevatedButton(
               onPressed: () {
-                // Ajoutez ici la logique d'authentification avec Facebook
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                );
               },
-              child: const Text('Login with Facebook'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.black,
+                onPrimary: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.facebook,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 8.0),
+                  Text(
+                    'Login with Facebook',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
