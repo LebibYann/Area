fb5bcac895d969ea98b40ebadb446dde98be984b
MAUREL Valentin <valentin1.maurel@epitech.eu>
2023-12-09 01:30:31 +0100

    docs(server): :memo: update swagger api doc

--- server/src/modules/users/dtos/updateUser.dto.ts
+++ server/src/modules/users/dtos/updateUser.dto.ts
@@ -1,12 +1,15 @@
 import { IsEmail, IsOptional, IsString, MinLength } from '@nestjs/class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto {
   @IsEmail()
   @IsOptional()
+  @ApiProperty({ example: 'user@example.com', description: 'The email of the User' })
   email?: string;
 
   @IsString()
   @MinLength(8)
   @IsOptional()
+  @ApiProperty({ example: 'password', description: 'The password of the User' })
   password?: string;
 }
--- server/src/modules/users/users.controller.ts
+++ server/src/modules/users/users.controller.ts
@@ -13,33 +13,57 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { User } from './users.entity';
 import { UpdateUserDto } from './dtos/updateUser.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Post()
     @HttpCode(HttpStatus.CREATED)
+    @ApiOperation({ summary: 'Create user' })
+    @ApiResponse({ status: HttpStatus.CREATED, description: 'The user has been successfully created.'})
+    @ApiResponse({ status: HttpStatus.CONFLICT, description: 'The email is already taken.'})
+    @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'The email or password is invalid.'})
     async create(@Body() createUserDto: CreateUserDto): Promise<User> {
         return this.usersService.create(createUserDto);
     }
 
     @Get()
+    @ApiOperation({ summary: 'Get all users' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'All users' })
+    @ApiResponse({ status: HttpStatus.NO_CONTENT, description: 'No users found' })
+    @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: 'Unauthorized' })
     async findAll(): Promise<User[]> {
         return this.usersService.findAll()
     }
 
     @Get(':id')
+    @ApiOperation({ summary: 'Get user by id' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'The user with the matching id' })
+    @ApiResponse({ status: HttpStatus.NO_CONTENT, description: 'No user found with the matching id' })
+    @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: 'Unauthorized' })
     async findOne(@Param('id') id: number): Promise<User> {
         return this.usersService.findOne(id);
     }
 
     @Delete(':id')
+    @ApiOperation({ summary: 'Delete user by id' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'The user with the matching id has been deleted' })
+    @ApiResponse({ status: HttpStatus.NO_CONTENT, description: 'No user found with the matching id' })
+    @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: 'Unauthorized' })
     async remove(@Param('id') id: number): Promise<void> {
         return this.usersService.remove(id);
     }
 
     @Patch(':id')
+    @ApiOperation({ summary: 'Update user by id' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'The user with the matching id has been updated' })
+    @ApiResponse({ status: HttpStatus.NO_CONTENT, description: 'No user found with the matching id' })
+    @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'The email or password is invalid.'})
+    @ApiResponse({ status: HttpStatus.CONFLICT, description: 'The email is already taken.'})
+    @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: 'Unauthorized' })
     async update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto): Promise<void> {
         return this.usersService.update(id, updateUserDto);
     }
