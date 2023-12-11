0704f7fff97ddd5ceed8af8cbd6bd635a15a8eb9
Joseph <joseph.susanyan@epitech.eu>
2023-12-11 02:21:04 +0100

    feat(pages): Add the front for the explore page

--- mobile/lib/pages/home/explore.dart
+++ mobile/lib/pages/home/explore.dart
@@ -2,11 +2,76 @@ import 'package:flutter/material.dart';
 
 class ExplorePage extends StatelessWidget {
   const ExplorePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Explore Page Content'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "All (0)",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Divider(
+            height: 20,
+            indent: 20,
+            endIndent: 20,
+            color: Colors.grey,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Container(
+              margin: const EdgeInsets.only(top: 20.0),
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(13.0),
+                color: Colors.black,
+              ),
+              child: const Text(
+                'Save time and money by making the internet work for you! We believe you might like...',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+            child: InkWell(
+              onTap: () {
+                print('You want to create your applet');
+              },
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(13.0),
+                  color: Colors.black,
+                ),
+                child: const Text(
+                  'Create your applet',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
