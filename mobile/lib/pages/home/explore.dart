7c11fe4d667449b07b64d32e1f878c6130ac0e84
Joseph <joseph.susanyan@epitech.eu>
2023-12-13 19:58:32 +0100

    style(mobile): Resize services add more detail for a better render

--- mobile/lib/pages/home/explore.dart
+++ mobile/lib/pages/home/explore.dart
@@ -8,28 +8,47 @@ import 'package:mobile/pages/AREAS/gmail.dart';
 
 class ExplorePage extends StatelessWidget {
   ExplorePage({Key? key}) : super(key: key);
-  final List<String> areaNames = ['instagram', 'facebook', 'discord', 'spotify', 'gdrive', 'gmail'];
+  final List<String> areaNames = [
+    'Instagram',
+    'Facebook',
+    'Discord',
+    'Spotify',
+    'Gdrive',
+    'Gmail'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AREAS'),
+        title: const Text('AREAS',
+          style: TextStyle(fontWeight: FontWeight.bold),),
+        centerTitle: true,
       ),
       body: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 8.0,
-          mainAxisSpacing: 8.0,
+          crossAxisCount: 4,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
         ),
         itemCount: areaNames.length,
         itemBuilder: (context, index) {
-          return InkWell(
-            onTap: () {
-              if (areaNames[index] == 'instagram') {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                areaNames[index],
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Expanded(
+                child: InkWell(
+                  onTap: () {
+                    if (areaNames[index] == 'instagram') {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const InstagramAREA()),
+                  MaterialPageRoute(
+                      builder: (context) => const InstagramAREA()),
                 );
               } else if (areaNames[index] == 'facebook') {
                 Navigator.push(
@@ -41,13 +60,13 @@ class ExplorePage extends StatelessWidget {
                   context,
                   MaterialPageRoute(builder: (context) => const DiscordAREA()),
                 );
-              } else if(areaNames[index] == 'spotify') {
-                  Navigator.push(
+              } else if (areaNames[index] == 'spotify') {
+                Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const SpotifyAREA()),
                 );
-              } else if(areaNames[index] == 'gdrive') {
-                  Navigator.push(
+              } else if (areaNames[index] == 'gdrive') {
+                Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const GdriveAREA()),
                 );
@@ -57,13 +76,24 @@ class ExplorePage extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => const GmailAREA()),
                 );
               }
-            },
-            child: Card(
-              child: Image.asset('assets/AREA/${areaNames[index]}.png'),
-            ),
+                  },
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset('assets/AREA/${areaNames[index].toLowerCase()}.png'),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           );
         },
       ),
     );
   }
 }
+
