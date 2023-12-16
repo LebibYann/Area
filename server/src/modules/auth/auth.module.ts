f31dbcaf1d0ed791c16d453e60de5feb60ce0bae
MAUREL Valentin <valentin1.maurel@epitech.eu>
2023-12-16 13:01:33 +0100

    fix(server): Update auth module imports and file paths

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -4,9 +4,9 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import { AuthService } from './auth.service';
+import { AuthService } from './services/auth.service';
 import { UsersModule } from '../users/users.module';
-import { AuthController } from './auth.controller';
+import { AuthController } from './controllers/auth.controller';
 
 @Module({
   imports: [
