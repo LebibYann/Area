e0cb28359aec1ded52e9a421d8b589f597acf095
MAUREL Valentin <valentin1.maurel@epitech.eu>
2023-12-16 12:57:36 +0100

    refactor(server): Refactor authentication strategies

--- server/src/modules/auth/strategies/jwt.strategy.ts
+++ server/src/modules/auth/strategies/jwt.strategy.ts
@@ -4,6 +4,8 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UsersService } from '../../users/users.service';
 import { ConfigService } from '@nestjs/config';
+import { User } from 'src/modules/users/users.entity';
+import { JwtPayload } from '../interfaces/jwt.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -18,7 +20,11 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
-    return await this.usersService.findOne(payload.sub);
+  async validate(payload: JwtPayload): Promise<User> {
+    if (payload.token_type === 'local') {
+      return this.usersService.findOneByEmail(payload.email);
+    } else if (payload.token_type === 'oauth2') {
+
+    }
   }
 }
