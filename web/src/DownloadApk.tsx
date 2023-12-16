5d9b1fb2a3713d99cf3469339f4b8d953c71c58b
LebibYann <yann.lebib@epitech.eu>
2023-12-16 17:33:48 +0100

    fix(web): :bug: Run time crash
    The web app no longer crashes because of an invalid function call

--- web/src/DownloadApk.tsx
+++ web/src/DownloadApk.tsx
@@ -4,13 +4,12 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const DownloadApk = (): JSX.Element => {
-    const file = new File(".babelrc")
     return (
         <div style={{
             display: 'flex',
             flexDirection: 'column',
         }}>
-            <a href={URL.createObjectURL(".babelrc")} download>Download</a>
+            <a href={"/app-release.apk"} download>Download</a>
         </div>
     );
 }
