7e53d353fa68882faa47dc6b4f44a504eec4c439
MAUREL Valentin <valentin1.maurel@epitech.eu>
2023-12-16 14:01:18 +0100

    fix(server): Add OAuth2 functionality to AuthModule and establish a forwardRef relationship with UsersModule

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
@@ -7,22 +7,41 @@ import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthService } from './services/auth.service';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './controllers/auth.controller';
+import { CredentialService } from './services/credential.service';
+import { TokenService } from './services/token.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Credential } from './entities/credential.entity';
+import { Token } from './entities/token.entity';
+import { OAuth2Controller } from './controllers/oauth2.controller';
+import { OAuth2Service } from './services/oauth2.service';
+import { GoogleOAuth2Service } from './services/google.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
-    UsersModule,
+    forwardRef(() => UsersModule),
+    TypeOrmModule.forFeature([Credential, Token]),
     PassportModule,
+    HttpModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-          secret: configService.get('JWT_SECRET'),
-          signOptions: { expiresIn: '60s' },
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '60s' },
       }),
       inject: [ConfigService],
     }),
   ],
-  controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
-  exports: [AuthService],
+  controllers: [AuthController, OAuth2Controller],
+  providers: [
+    AuthService,
+    CredentialService,
+    TokenService,
+    OAuth2Service,
+    GoogleOAuth2Service,
+    LocalStrategy,
+    JwtStrategy,
+  ],
+  exports: [AuthService, CredentialService, TokenService],
 })
-export class AuthModule {}
+export class AuthModule { }
--- server/src/modules/users/users.module.ts
+++ server/src/modules/users/users.module.ts
@@ -3,9 +3,13 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
+import { CredentialService } from '../auth/services/credential.service';
+import { TokenService } from '../auth/services/token.service';
+import { AuthModule } from '../auth/auth.module';
+import { forwardRef } from '@nestjs/common';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService]
