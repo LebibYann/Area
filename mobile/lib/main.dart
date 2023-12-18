328595eccdd948b1a857b39b150b1c30c7e43c1d
Enzo <enzo.pfeiffer@epitech.eu>
2023-12-18 19:08:20 +0100

    feat(init): intitialisation mobile featching about.json

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
-// import 'package:mobile/pages/home/home.dart';
 import 'package:mobile/pages/login/login.dart';
+import 'package:mobile/json.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await JsonDataSingleton().fetchData();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
