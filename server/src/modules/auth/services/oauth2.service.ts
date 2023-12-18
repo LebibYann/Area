68451f3091e633d30e47628b1f06d83d4d4a7685
Valentin MAUREL <valentin.maurel@orange.fr>
2023-12-18 19:22:12 +0100

    fix(server): casing in API response description

--- server/src/modules/auth/services/oauth2.service.ts
+++ server/src/modules/auth/services/oauth2.service.ts
@@ -7,15 +7,19 @@ import { Token } from '../entities/token.entity';
 import { TokenResponse } from '../interfaces/token.interface';
 import { UserInfoResponse } from '../interfaces/userInfo.interface';
 import { UsersService } from 'src/modules/users/users.service';
+import { Logger } from '@nestjs/common';
+import { AxiosError } from 'axios';
 
 @Injectable()
 export class OAuth2Service {
   constructor(
-    private httpService: HttpService,
-    private userService: UsersService,
-    private credentialService: CredentialService,
-    private tokenService: TokenService,
+    protected httpService: HttpService,
+    protected userService: UsersService,
+    protected credentialService: CredentialService,
+    protected tokenService: TokenService,
   ) { }
+
+  private readonly logger = new Logger(OAuth2Service.name);
 
   async exchangeCodeForToken(
     service: string,
@@ -26,14 +30,19 @@ export class OAuth2Service {
     redirectUri: string
   ): Promise<Token> {
     try {
-      const response = await firstValueFrom(this.httpService.post(tokenEndpoint, {
-        code,
-        client_id: clientId,
-        client_secret: clientSecret,
-        redirect_uri: redirectUri,
-        grant_type: 'authorization_code',
-      }));
+      // Request token from service
+      const response = await firstValueFrom(this.httpService.post(
+        tokenEndpoint,
+        {
+          code,
+          client_id: clientId,
+          client_secret: clientSecret,
+          redirect_uri: redirectUri,
+          grant_type: 'authorization_code',
+        }
+      ));
 
+      // Create token entity
       const token = await this.tokenService.create({
         service,
         accessToken: response.data.access_token,
@@ -41,10 +50,15 @@ export class OAuth2Service {
       });
 
       return token;
+
     } catch (error) {
+      if ( error instanceof AxiosError )
+        this.logger.error("Error exchanging code for token:", error.response.data);
+      else
+        this.logger.error("Error exchanging code for token:", error);
       throw new HttpException(
         'Failed to exchange code for token',
-        HttpStatus.INTERNAL_SERVER_ERROR
+        HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
   }
@@ -56,6 +70,7 @@ export class OAuth2Service {
     clientSecret: string
   ): Promise<TokenResponse> {
     try {
+      // Refresh token from service
       const response = await firstValueFrom(this.httpService.post(tokenEndpoint, {
         refresh_token: refreshToken,
         client_id: clientId,
@@ -65,6 +80,10 @@ export class OAuth2Service {
 
       return response.data;
     } catch (error) {
+      if ( error instanceof AxiosError )
+        this.logger.error("Error refreshing token:", error.response.data);
+      else
+        this.logger.error("Error refreshing token:", error);
       throw new HttpException(
         'Failed to refresh token',
         HttpStatus.INTERNAL_SERVER_ERROR
@@ -77,6 +96,7 @@ export class OAuth2Service {
     userInfoEndpoint: string
   ): Promise<UserInfoResponse> {
     try {
+      // Request user info from service
       const response = await firstValueFrom(this.httpService.get(userInfoEndpoint, {
         headers: {
           Authorization: `Bearer ${accessToken}`
@@ -85,6 +105,11 @@ export class OAuth2Service {
 
       return { id: response.data.sub, email: response.data.email }
     } catch (error) {
+      if ( error instanceof AxiosError ) {
+        this.logger.error("Error validating user:", error.response.data);
+      } else {
+        this.logger.error(error);
+      }
       throw new HttpException(
         'Failed to validate user',
         HttpStatus.INTERNAL_SERVER_ERROR
@@ -96,6 +121,4 @@ export class OAuth2Service {
     const user = await this.userService.findOneByEmail(email);
     return !!user;
   }
-
-  
 }
