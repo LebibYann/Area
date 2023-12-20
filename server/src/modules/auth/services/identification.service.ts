120d6228158c477f933698d6b23581f343647ded
Valentin MAUREL <valentin.maurel@orange.fr>
2023-12-20 19:33:40 +0100

    feat(server): Add logging to IdentificationService

--- server/src/modules/auth/services/identification.service.ts
+++ server/src/modules/auth/services/identification.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { AccessTokenResponse } from "../interfaces/accessTokenRes.interface";
 import { CredentialService } from "./credential.service";
 import { Credential } from "../entities/credential.entity";
@@ -12,13 +12,17 @@ export class IdentificationService {
     private readonly userService: UsersService,
   ) { }
 
+  logger = new Logger(IdentificationService.name);
+
   async identifyCredentials(
     service: string,
     token: AccessTokenResponse,
     user: User,
   ): Promise<Credential> {
+    this.logger.debug("Identify Credentials", { service, token, user });
     const credential = await this.credentialService.findOneByUserAndService(user, service);
     if (credential) {
+      this.logger.debug("Credentials found");
       await this.credentialService.update(credential.id, {
         accessToken: token.access_token,
         refreshToken: token.refresh_token,
@@ -26,6 +30,7 @@ export class IdentificationService {
         expiresAt: new Date(Date.now() + token.expires_in * 1000),
       });
     } else {
+      this.logger.debug("Credentials not found. Creating...")
       await this.credentialService.create(
         user.id,
         service,
