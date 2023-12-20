6002aa7e59ce1de94732d1e16f1ed9855394a592
Valentin MAUREL <valentin.maurel@orange.fr>
2023-12-20 19:33:57 +0100

    feat(server): Add credential service and implement token validation

--- server/src/modules/auth/strategies/jwt.strategy.ts
+++ server/src/modules/auth/strategies/jwt.strategy.ts
@@ -7,11 +7,13 @@ import { ConfigService } from '@nestjs/config';
 import { User } from 'src/modules/users/users.entity';
 import { JwtPayload } from '../interfaces/jwt.interface';
 import { Logger } from '@nestjs/common';
+import { CredentialService } from '../services/credential.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     private usersService: UsersService,
+    private readonly credentialService: CredentialService,
     private configService: ConfigService
   ) {
     super({
@@ -30,10 +32,26 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         this.logger.warn('Cannot find user associated with this token.', payload);
         throw new UnauthorizedException('Invalid token.');
       }
+      const credential = await this.credentialService.findOneByUserAndService(user, 'local');
+      if (!credential) {
+        this.logger.warn('Cannot find credential associated with this token.', payload);
+        throw new UnauthorizedException('Invalid token.');
+      }
+      if (credential.validatePassword(payload.sub)) {
+        this.logger.warn('Invalid password.', payload);
+        throw new UnauthorizedException('Invalid token.');
+      }
       return user;
     } else if (payload.token_type === 'oauth2') {
-      // TODO: Implement OAuth2 validation
-      throw new NotImplementedException('OAuth2 validation is not implemented yet.');
+      const user = await this.usersService.findOneByEmail(payload.email);
+      if (!user) {
+        this.logger.warn('Cannot find user associated with this token.', payload);
+        throw new UnauthorizedException('Invalid token.');
+      }
+      if (payload.sub !== user.id.toString()) {
+        this.logger.warn('Invalid user ID.', payload);
+        throw new UnauthorizedException('Invalid token.');
+      }
     }
     throw new BadRequestException('Invalid token type. The token type must be either "local" or "oauth2".');
   }
