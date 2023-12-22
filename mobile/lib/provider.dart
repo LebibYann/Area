b5682d92dc456e3b93bc766c8e1703d64806bb51
Joseph <joseph.susanyan@epitech.eu>
2023-12-22 16:36:01 +0100

    feat: Add the image attribute to the provider

--- mobile/lib/provider.dart
+++ mobile/lib/provider.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class AuthState with ChangeNotifier {
   String _accessToken = "";
   String _email = "";
+  String? _profileImagePath;
 
   String get accessToken => _accessToken;
   String get email => _email;
+  String? get profileImagePath => _profileImagePath;
 
   set accessToken(String token) {
     _accessToken = token;
@@ -14,6 +16,11 @@ class AuthState with ChangeNotifier {
 
   set email(String email) {
     _email = email;
+    notifyListeners();
+  }
+
+  set profileImagePath(String? path) {
+    _profileImagePath = path;
     notifyListeners();
   }
 }
