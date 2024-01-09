baee79e910089e46f2c76af2ae743f6d3d500e35
LebibYann <yann.lebib@epitech.eu>
2024-01-09 11:57:58 +0100

    feat(web): :lipstick: Services style added
    Icons and css of certains services added

--- web/src/services/ServiceCard.tsx
+++ web/src/services/ServiceCard.tsx
@@ -3,6 +3,9 @@ import google from '../assets/images/GoogleIcon.png'
 import discord from '../assets/images/DiscordIcon.png'
 import spotify from '../assets/images/SpotifyIcon.png'
 import instagram from '../assets/images/InstagramIcon.png'
+import github from '../assets/images/GithubIcon.png'
+import twitter from '../assets/images/TwitterIcon.png'
+import gmail from '../assets/images/GmailIcon.png'
 import './ServiceCard.css'
 
 const useServiceIcon = (name: string): string | undefined => {
@@ -15,6 +18,12 @@ const useServiceIcon = (name: string): string | undefined => {
       return (spotify)
     case 'instagram':
       return (instagram)
+    case 'github':
+      return (github)
+    case 'twitter':
+      return (twitter)
+    case 'gmail':
+      return (gmail)  
     default:
       return (undefined)
   }
